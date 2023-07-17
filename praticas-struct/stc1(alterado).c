3ee77e0605f3a12c699379724789f1aa89ff0296
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-17 14:52:20 -0300

    Create stc1(alterado).c

---
+++ praticas-struct/stc1(alterado).c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Pragrama que cadastra funcionarios 
+
+// Passo 1 - Definir a struct que representa o funcionario
+
+typedef struct funcionario{
+    char nome[20];
+    int idade;
+    float salario;
+    char cargo[20];
+}Funcionario;
+
+void Funcionario * cadastraFuncionario (void){
+/* Função que cadastra funcionário */
+
+    int quantidade_funcionarios;
+        printf("Informe a quantidade de funcionários a ser cadastrado: \n");
+        scanf("%d", &quantidade_funcionarios);
+
+// Declaração de vetor do tipo funcionario
+
+    Funcionario * vetor = (Funcionario *)malloc(quantidade_funcionarios*sizeof(Funcionario));
+        if (vetor == NULL){
+            prinft("Sem memória!\n");
+            exit(1);
+    }
+
+// Preenche o vetor de funcionario
+    int indice;
+    for (indice=0; indice< qnt_funcionarios; indice++){
+        printf("Digite o nome: \n");
+        scanf(" %[^\n]", vetor_funcionario[indice].nome);
+        printf("Digite a idade: \n");
+        scanf("%d", &vetor_funcionario[indice].idade);
+        printf("Digite o salário: \n");
+        scanf("%f", &vetor_funcionario[indice].salario);
+        printf("Digite o cargo: \n");
+        scanf(" %[^\n]", vetor_funcionario[indice].cargo);
+    }
+
+    return vetor_funcionario;
+}
+
+int main (void){
+    
+    int qntFun;
+    printf("Informe a quantidade de funcionários a ser cadastrado: \n");
+    scanf("%d", &qntFun);
+    
+    funcionario * cadastraFuncionario;
+    imprime(vetor_funcionario, qntFun);
+
+    return 0;
+}
+ // Passo 3 - Função que imprime os dados dos funcionarios
+void imprime(Funcionario * vetor_funcionario, int qnt_funcionarios){
+    int indice; 
+    prinft("Dados dos funcionários: \n")
+    for (indice=0; indice< qnt_funcionarios; indice++){
+        printf("Os dados são:\n Nome: %s\n Idade: %d \n Salário: %.2f \n Cargo: %s\n", vetor_funcionario[indice].nome, vetor_funcionario[indice].idade, vetor_funcionario[indice].salario, vetor_funcionario[indice].cargo);
+        
+    }
+
+}
