c78fffabdb60286c524e618630e4a2ebeeb6fe16
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-25 17:18:06 -0300

    Create Questão3.c

---
+++ Union/Struct_union_enum/Questão3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//precisa terminar!!!
+
+typedef struct data{
+    int dia;
+    char mes[20];
+    int ano;
+}Data;
+
+enum meses {
+    janeiro, fevereiro, marco, abril, maio, junho, julho,
+    agosto, setembro, outubro, novembro, dezembro
+};
+
+int main (void){
+    
+    return 0;
+}
