277216c2e8f71e8085f54ef20f138191c98c257e
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 18:10:44 -0300

    Update aloc1.c

--- Ponteiros/aloc1.c
+++ Ponteiros/aloc1.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 
 int main (void){
-    int tamanho;
+    int tamanho; // declaração da variável 'tamanho' que armazenará o tamanho do vetor 
     printf ("Digite o tamanho do vetor\n");
     scanf ("%d", &tamanho);
 
-    int * vetor = (int*) malloc(tamanho*sizeof(int));
+    int * vetor = (int*) malloc(tamanho*sizeof(int)); // realização da alocação dinâmica na memória 
     if (vetor == NULL) {
-    printf ("Erro de alocação \n");
+    printf ("Erro de alocação \n"); // caso haja erros, a alocação será abortada
     exit (1);
     }
     else {
         printf (" Alocação realizada com sucesso! \n");
     }
+    
     // Recebe elementos inseridos pelo user
     int contador;
     printf ("Digite os valores do vetor: \n");
-    for (contador=0 ; contador<tamanho; contador++);{
-        scanf ("%d", &vetor[contador]);
+    for (contador=0 ; contador<tamanho; contador++){
+        scanf ("%d", vetor[contador]);
     }
 
     for (contador = tamanho - 1; contador >= 0; contador--){
     printf("%d", &vetor[contador]);
     }
-    free (vetor);
+    free (vetor); // liberação da memória
     return 0;
 }
