a4308cced3fb1aee5a5eaa92098c762618fda0bf
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 16:28:20 -0300

    Create Ativ1.c

---
+++ Ponteiros/Ativ1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// ponteiros
+int main(){
+        int x=2; // declarando uma variável e atribuindo valor à ela
+        int *p= &x; // dando ao ponteiro o valor que a variável x tem
+        return 0;
+}
