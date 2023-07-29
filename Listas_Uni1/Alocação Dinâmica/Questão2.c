6adbd602cfd031af5a9d01d5e954f88f1929d4c6
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 19:18:22 -0300

    Update Questão2.c

--- Listas_Uni1/Alocação Dinâmica/Questão2.c
+++ Listas_Uni1/Alocação Dinâmica/Questão2.c
@@ -2,35 +2,37 @@
 
 int main (void){
     int n, notatotal = 0, aprovados = 0;
-    printf("Número de questões: ");
+    printf("Número de questões: "); // solicita ao usuário o número de questões do gabarito
     scanf("%d", &n);
-    char gab[n]; 
-    int alunos[10], nota[10/n];
+    char gab[n]; // vetor para armazenar o gabarito 
+    int alunos[10], nota[10]; // vetor 'alunos' para guardar o número dos alunos e 'nota' para guardar a nota de cada aluno
     
-    for (int x = 0; x < n; x++){
-        printf("Resposta: %i", x+1);      //informa a reposta da questão
+    for (int x = 0; x < n; x++){ // preenche o gabarito das questões
+        printf("Resposta: %i", x + 1);      //informa a reposta da questão
         gab[x] = getche();          //armazena a letra no gabarito
+        printf("\n");
     }
 
-    for (int x = 0; x < 10; x++){
-        printf("Informe o numero do aluno: ");
+    for (int x = 0; x < 10; x++){ // calcula a nota de cada aluno
+        printf("Informe o número do aluno: ");
         scanf("%i", &alunos[x]);
-        notatotal=0;
+        notatotal = 0;
         for (int a = 0; a < n ; a++){
-            printf("Resposta aluno: %i", x+1); //respostas dos alunos
-            if (gab[a] == getche()) {
+            printf("Resposta do aluno %i para a questão %i", aluno[x], a + 1); //respostas dos alunos
+            if (gab[a] == getche()) { // aqui faz a verificação se a resposta do aluno é a do gabarito
                 notatotal++; 
         }
+            printf("\n");
     }
-    nota[x] = notatotal; //o aluno da volta do laço vai receber a nota que obteve
+    nota[a] = notatotal; // a nota do aluno será armazenada no indice correspondente 
 }
-    for (int a = 0; a < 10; a++){
-        printf("Aluno %i nota = %f", alunos[a], nota[a]);
+    for (int a = 0; a < 10; a++){ // faz a impressão das notas dos alunos e calcula o total de aprovados 
+        printf("Aluno %i nota = %d", alunos[a], nota[a]);
         if (nota[a] >= 6){
             aprovados++;
         }
     }
 
-    printf("%% de aprovados = %f", (aprovados*100)/10);
+    printf("%% de aprovados = %d\n", (aprovados*100)/10);
     return 0;
 }
