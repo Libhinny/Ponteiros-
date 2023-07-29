a3d03eb59338c48d30899d5601cca6f572038065
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 19:47:03 -0300

    Update Questão3.c

--- Listas_Uni1/Vetores e Matrizes/Questão3.c
+++ Listas_Uni1/Vetores e Matrizes/Questão3.c
@@ -3,18 +3,18 @@
 #include <time.h>
 
 int main (void){
-    int n;
-    printf("Informe um número inteiro positivo: "); //ordem da matriz definada pelo user
-    scanf("%d", &n);
+    int n; // declaração da variável 'n'
+    printf("Informe um número inteiro positivo: "); // ordem da matriz definada pelo user
+    scanf("%d", &n); // armazena o valor na variável 'n'
     
     int linhas = n, colunas = n, i, j;
     
-    int **matriz = (int **) malloc (linhas*sizeof(int*)); //alocação de cada linha
+    int **matriz = (int **) malloc (linhas*sizeof(int*)); //alocação dinamica de cada linha
     for (i = 0; i< linhas; i++){
         matriz[i] = (int *)malloc(colunas*sizeof(int));
     }
     
-    srand(time(NULL)); //responsavel por alimentar a rand
+    srand(time(NULL)); // responsavel por alimentar a rand com valores aleatórios 
    
     for (i = 0; i < n; i++){
         for (j = 0; j < n; j++){
@@ -26,6 +26,11 @@ for (i = 0; i < n; i++){
         for (j = 0; j < n; j++)
         printf("%d", matriz[i][j]);
         printf("\n");
+    }
+
+    // libera a memória alocada para cada linha da matriz
+    for (i = 0; i < linhas; i++){
+        free(matriz[i]);
     }
 
     free(matriz);
