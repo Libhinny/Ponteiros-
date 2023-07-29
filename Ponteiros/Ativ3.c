c9944a7ed958eb6b20dcd8933b65425589befd99
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 18:05:31 -0300

    Update Ativ3.c

--- Ponteiros/Ativ3.c
+++ Ponteiros/Ativ3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-    int soma (int valor1, int valor2){
-        int sum = valor1 + valor2;
+    int soma (int valor1, int valor2){ // nesta função, temos dois argumentos do tipo inteiro
+        int sum = valor1 + valor2; // e ele vai realizar a soma desses dois valores
         return sum;
     }
 
 int main(void){
     
-int (*p)(int, int) = soma;
-int resultado = p(2,3);
-printf ("Valor da soma = %d \n", resultado);
+int (*p)(int, int) = soma; // aqui este ponteiro aponta para a função soma
+int resultado = p(2,3); // chama a função através do ponteiro 'p' e a soma é armazenada na variável 'resultado'
+printf ("Valor da soma = %d \n", resultado); // imprime o resultado da soma
    
     return 0;
 }
