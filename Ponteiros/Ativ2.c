bce78234ed0a39497755de8c7599d3ea315c81c8
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 17:59:11 -0300

    Update Ativ2.c

--- Ponteiros/Ativ2.c
+++ Ponteiros/Ativ2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void incrementa (int * ponteiro){
-    (*ponteiro)++;
+void incrementa (int * ponteiro){ // recebe um ponteiro de inteiro e vai armazenar o endereço de uma variável do tipo inteiro
+    (*ponteiro)++; // aqui ele vai fazer o incremento, além disso, o * neste caso faz com que peguemos o valor armazenado no endereço
 }
 int main(void){
     
-    int var = 2;
-    printf("var = %d \n", var);
-    incrementa (&var);
-    printf("var = %d\n", var);
+    int var = 2; // declaração da variável
+    printf("var = %d \n", var); // imprime o valor da variável
+    incrementa (&var); // chama a função e ela faz o incremento do valor associado a variável
+    printf("var = %d\n", var); // imprime a variável depois de ela ser incrementada
    
     return 0;
 }
