d546fe3d49aca555566abc86d7428a7490c8144b
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 17:45:52 -0300

    Update Ativ1-1.c

--- Ponteiros/Ativ1-1.c
+++ Ponteiros/Ativ1-1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main (void){
- float var = 2.5;
-float *ponteiro = &var;
-printf ("var = %f \n ponteiro = %p", var, ponteiro);
+ float var = 2.5; // declaração da varíável var
+float *ponteiro = &var; // daclaração do ponteiro que aponta para o endereço da variável var
+printf ("var = %f \n ponteiro = %p", var, ponteiro); // aqui vai exibir o valor de var e o endereço de memória apontado
 return 0;
 }
