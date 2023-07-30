3cc4bfbf84b30e55bbd1cb6467150c11b4e0350d
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 12:43:59 -0300

    Update pratica4.c

--- praticas-struct/pratica4.c
+++ praticas-struct/pratica4.c
@@ -1,19 +1,19 @@
 //Programa que recebe dados de uma conta bancaria
 #include <stdlib.h>
 
-typedef struct conta {
+typedef struct conta { // struct com os campos de nome, agencia, numero da conta, senha e saldo 
     char nome[20];
     int agencia;
     int numConta;
     char senha;
     float saldo;
-}Conta;
+}Conta; // renomea com o comando typedef
 
 int main (void){
-    Conta var_conta[5];
+    Conta var_conta[5]; // declara o vetor com tamanho 5
 
-    int indice;
-    for (indice=0; indice<5; indice++){
+    int indice; 
+    for (indice=0; indice<5; indice++){ // laço para percorrer todo o vetor e pegar as informações da conta de determinado usuário
 
     printf("Informe o nome: ");
     scanf(" %[^\n]", var_conta[indice].nome);
@@ -25,13 +25,14 @@ int main (void){
     scanf("%d", &var_conta[indice].numConta );
 
     printf("Digite a senha: ");
-    scanf("%f", &var_conta[indice].senha );
+    scanf("%c", &var_conta[indice].senha );
 
     printf("Digite  saldo: ");
     scanf("%f", &var_conta[indice].saldo );
     }
-
-    printf("Os dados informados:\n Nome: %s \t Agencia: %d \t Número da conta: %d \t Senha: %s \t Saldo: %2.f", var_conta.nome, var_conta.agencia, var_conta.numConta, var_conta.senha, var_conta.saldo);
-
+    
+    for (indice = 0; indice < 5; indice++){ // percorre o laço e faz a impressão dos dados da conta do user
+        printf("Os dados informados:\n Nome: %s \t Agencia: %d \t Número da conta: %d \t Senha: %s \t Saldo: %2.f", var_conta.nome, var_conta.agencia, var_conta.numConta, var_conta.senha, var_conta.saldo);
+    }
     return 0; 
 }
