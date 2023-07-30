b8225cb5734b7baa03585a487537b3282ee4946b
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 19:04:52 -0300

    Update Questão3.c

--- Union/Struct_union_enum/Questão3.c
+++ Union/Struct_union_enum/Questão3.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 
-//precisa terminar!!!
+// representa os meses
+typedef enum {
+    JANEIRO = 1, FEVEREIRO, MARCO, ABRIL, 
+        MAIO, JUNHO, JULHO, AGOSTO, 
+    SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+}Mes; // renomea com o comando typedef
 
-typedef struct data{
+typedef struct data{ // struct que armazenar a data
     int dia;
-    char mes[20];
+    Mes mes;
     int ano;
-}Data;
-
-enum meses {
-    janeiro, fevereiro, marco, abril, maio, junho, julho,
-    agosto, setembro, outubro, novembro, dezembro
-};
+}Data; // renomea com o comando typedef
 
 int main (void){
-    
+    Data data; // declara a variavel data do tipo 'Data'
+
+    // leitura da data
+    printf("Digite o dia: ");
+    scanf("%d", &data.dia);
+
+    printf("Digite o mes (1 a 12): ");
+    int mes;
+    scanf("%d", &mes);
+
+    // verificação se o mês está dentro do intervalo válido
+    if (mes < JANEIRO || mes > DEZEMBRO) {
+        printf("Mes invalido!\n"); // se não, é abortado
+        return 1;
+    }
+
+    data.mes = (Mes)mes;
+
+    printf("Digite o ano: ");
+    scanf("%d", &data.ano);
+
+    // imprime da data no formato "dd/mm/aaaa"
+    printf("Data: %02d/%02d/%04d\n", data.dia, data.mes, data.ano);
+
     return 0;
 }
