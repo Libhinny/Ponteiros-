d1c6942c892935ddb244badba7588ee46f0e2a1d
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 12:09:32 -0300

    Update atividade_struct1.c

--- praticas-struct/atividade_struct1.c
+++ praticas-struct/atividade_struct1.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct dados{   // Struct e Ponteiros
+typedef struct dados{   // struct dados para o armazenamento do nome, salario, id e cargo do usuário
     char nome[30];
     float salario;
     int identificador;
     char cargo;
 }Dados;
 
-Dados * cadastro_funcionário (void); 
+Dados * cadastro_funcionario (void); 
 
 int main (void){
+    Dados * funcionario = cadastro_funcionario();
+
+    // imprime os dados do funcionario cadastrado
+    printf("Dados do funcionário cadastrado: \n");
+    printf("Nome: %s\n", funcionario->nome);
+    printf("Salário: %2.f\n", funcionario->salario);
+    printf("Identificador: %d\n", funcionario->identificador);
+    printf("Cargo: %c\n", funcionario->cargo);
+
+    free(funcionario); // libera a memoria alocada
+    
     return 0;
 }
 
-Dados * cadastro_funcionário (void) {
-    Dados * dados = (Dados *) malloc (sizeof(Dados));
-    if (Dados == NULL){
+// função que realiza o cadastro dos funcionarios 
+Dados * cadastro_funcionario (void) {
+    Dados * dados = (Dados *) malloc (sizeof(Dados)); // alocação dinamica da memoria
+    if (dados == NULL){ // verificação se a alocação ocorreu corretamente
         printf("Sem memória");
         exit(1);
     }
+    // pega as info do usuário e as armazena
     prinft("Digite o nome: \n");
     scanf(" %[^\n]", dados->nome);
     printf("Informe o salário: \n");
@@ -27,6 +40,8 @@ Dados * cadastro_funcionário (void) {
     printf("Informe a numeração do identificador: \n");
     scanf("%d", &dados->identificador);
     prinft("Informe o cargo: \n");
-    scanf(" %[^\n]", dados->cargo)
+    scanf(" %[^\n]", dados->cargo);
+
+    return dados;
 
 }
