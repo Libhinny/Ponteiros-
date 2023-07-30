ce10b7bbeb19dc87d33a10d21f18607b87246b74
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 23:29:54 -0300

    Update union4.c

--- Union/union4.c
+++ Union/union4.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-
 /*
 Enumeração (enum)
 
@@ -15,24 +12,25 @@ enum bool{
 
 //essencial para se trabalhar com constantes
 
-enum boolean {false, true};
+enum boolean {false, true}; // def. da enum booleana com os argumentos false e true
 
-typedef enum boolean Bool;
+typedef enum boolean Bool; // renomeação da enum
 
 int main (void){
 
-    Bool val;
-    printf("Digite 0 ou 1: ");
-    scanf("%d", &val);
-    printf("Valor informado: %d", &val);
+    Bool val; 
+    printf("Digite 0 ou 1: "); // pede que o user informe um valor
+    scanf("%d", &val); // armazena o valor informado
+    printf("Valor informado: %d", &val); // exibe o valor informado
 
+    // verifica se o valor informado é verdadeiro ou falso
     if (val == true){
-        prinft("Verdadeiro");
+        prinft("Verdadeiro\n");
     } else if (val==false){
-        printf("False");
+        printf("False\n");
     }
-    else{
-        printf("Erro");
+    else{ // se o valor não for 0 ou 1, imprime a mensagem abaixo
+        printf("Erro\n");
     }
 
     return 0;
