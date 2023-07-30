c0eefc1ea9b3707a59247d937205f768c82255c2
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 13:20:59 -0300

    Update struct_ponteiros4.c

--- praticas-struct/struct_ponteiros4.c
+++ praticas-struct/struct_ponteiros4.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct disciplina{   // Struct e Ponteiros
+// Struct e Ponteiros
+
+/* É definido as duas estruturas de diciplina e aluno, para pegar as informações dos alunos */
+typedef struct disciplina{   
     char nome[30];
     float nota;
-}Disciplina;
+}Disciplina; // renomea com o comando typedef
 
 typedef struct aluno {
     char nome[20];
     int idade;
     Disciplina * disciplina;
-} Aluno;
+} Aluno; // renomea com o comando typedef
 
 int main (void){
-    Aluno * aluno = (Aluno *) malloc(sizeof(Aluno));
-    if (aluno == NULL){
+    Aluno * aluno = (Aluno *) malloc(sizeof(Aluno)); // faz a alocação dinamica de memória 
+    if (aluno == NULL){ // verifica se houve erros na alocação
         printf("Sem memória!\n");
         exit(1);
     }
 
+    // alocação dinamica de memoria para a variavel disciplina
     aluno->disciplina = (Disciplina*) malloc (sizeof(Disciplina)); //o sizeof não é multiplicado porque consideramos como se houvesse apenas 1 disciplina
-    if (aluno->disciplina == NULL){
+    if (aluno->disciplina == NULL){ // verifica se houve erros na alocação
         printf("Sem memória!\n");
         exit(1);
     }
 
+
+    // pega os dados fornecidos e os armazena 
     printf("Digite o nome: \n");
     scanf(" %[^\n]", aluno->nome );
 
@@ -37,8 +43,10 @@ int main (void){
     printf("Digite a nota: \n");
     scanf("%f", &aluno->disciplina->nota);
 
+    // faz a impressão dos dados do aluno
     printf("Dados: \n Nome: %s \n Idade: %d \n Disciplina: %s \n Nota: %2.f", aluno->nome, aluno->idade, aluno->disciplina->nome, aluno->disciplina.nota);
 
+    // antes de finalizar, faz a liberação da memoria para evitar vazamentos de memória
     free(aluno->disciplina);
     free(aluno);
     return 0;
