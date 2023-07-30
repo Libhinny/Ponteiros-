9f958ed1b85cf3fcaa4d1e9a07f3989a0546ab05
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 23:15:40 -0300

    Update union1.c

--- Union/union1.c
+++ Union/union1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-union documentos{
+union documentos{ // definição da union com os campos de cpf, rg e cnh 
     int cpf;
     int RG;
     int CNH;
 };
 
-typedef union documentos Docs;
+typedef union documentos Docs; // renomeação da union documentos para Docs
 
 int main(void){
-    Docs documento;
+    Docs documento; // def. da variável documento do tipo Docs
 
+    // pega os dados do user e os armazena 
     printf("Digite o CPF: \n ");
     scanf("%d", &documento.cpf);
     printf("Digite o RG: \n");
@@ -18,6 +19,7 @@ int main(void){
     printf("Digite o CNH: \n ");
     scanf("%d", &documento.CNH);
 
+    // imprime os dados armazenados na union
     printf("\n Documentos: \n CPF: %d RG: %d CNH: %d", documento.cpf, documento.RG, documento.CNH);
 
     return 0;
