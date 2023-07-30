17ffa78d29e201cddcf82dade63744db127d11a1
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 12:11:35 -0300

    Update pratica1.c

--- praticas-struct/pratica1.c
+++ praticas-struct/pratica1.c
@@ -1,15 +1,16 @@
 //Programa que manipula struct aluno
 #include <stdio.h>
 
-struct aluno {
+struct aluno { // struct que tem os campos dos dados do aluno
     char nome[20];
     int idade;
     int matricula;
 };
 
 int main (void){
-    struct aluno var_aluno;
+    struct aluno var_aluno; // declara a variavel var_aluno do tipo struct aluno
 
+    // pega os dados do aluno e os armazena
     printf("Digite o nome: ");
     scanf("%[^\n]", var_aluno.nome );
 
@@ -19,6 +20,7 @@ int main (void){
     printf("Digite a matricula: ");
     scanf("%d", &var_aluno.matricula );
 
+    // imprime os dados informados
     printf("Os dados informados:\n Nome: %s \t Idade: %d \t Matricula: %d ", var_aluno.nome, var_aluno.idade, var_aluno.matricula);
 
     return 0;
