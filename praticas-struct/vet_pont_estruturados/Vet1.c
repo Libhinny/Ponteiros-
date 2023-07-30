562f47012ccb31c13257e2edb215802efc9c3575
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 11:56:20 -0300

    Update Vet1.c

--- praticas-struct/vet_pont_estruturados/Vet1.c
+++ praticas-struct/vet_pont_estruturados/Vet1.c
@@ -9,15 +9,15 @@ typedef struct aluno{ /* declaração da função struct em que obtém as variá
     char tel[21];
 }Aluno;
 
-
-void inicializa (int n, Aluno** tab){ /*  */
+// as funções abaixo realizam operações com o vetor de ponteiros para alunos
+void inicializa (int n, Aluno** tab){ /* inicializa o vetor como se não houvesse alunos, definindo todos como null */
     int i;
     for (i=0; i < n; i++){
         tab[i] = NULL;
     }
 }
 
-void preenche (int n, Aluno** tab, int i){
+void preenche (int n, Aluno** tab, int i){ // preenche as info de um aluno em cada posição do vetor
     if (i < 0 || i >= n){
         printf ("Indice fora do limite do vetor.\n");
         exit(1); //aborta o programa
@@ -26,7 +26,7 @@ void preenche (int n, Aluno** tab, int i){
         tab[i] = (Aluno*)malloc(sizeof(Aluno)); // realização da alocação
     }
 
-    printf("Entre com a matricula: \n"); /* nessa parte o programa pegará as informações, de acordo com a quantidade de vezes em que o laço se repetirá */ 
+    printf("Entre com a matricula: \n"); /* nessa parte, o programa pegará as informações, de acordo com a quantidade de vezes em que o laço se repetirá */ 
     scanf("%d", &tab[i]->mat);
     printf("Entre com o nome: \n");
     scanf(" %80[^\n]", tab[i]->nome);
@@ -36,7 +36,7 @@ void preenche (int n, Aluno** tab, int i){
     scanf(" %20[^\n]", tab[i]->tel);
 }
 
-void retira (int n, Aluno** tab, int i){
+void retira (int n, Aluno** tab, int i){ // ela remove o aluno de uma determinada posição do vetor, liberando a memória
     if (i < 0 || i>= n){
         printf("Indice fora do limite do vetor. \n");
         exit(1); //aborta programa
@@ -47,7 +47,7 @@ void retira (int n, Aluno** tab, int i){
     }
 }
 
-void imprime (int n, Aluno** tab, int i){
+void imprime (int n, Aluno** tab, int i){ // exibe as info do aluno que está em uma posição do vetor
     if ( i < 0 || i >= n ){
         printf("Indice fora do limite do vetor.\n");
         exit(1);
@@ -60,21 +60,21 @@ void imprime (int n, Aluno** tab, int i){
     }
 }
 
-void imprime_tudo (int n, Aluno** tab){
+void imprime_tudo (int n, Aluno** tab){ // percorre o vetor e imprime as info de todos os alunos armazenados 
     int i;
     for (i = 0; i < n; i++){
         imprime (n, tab, i);
     }
 }
 
-int main (void){
-    Aluno* tab[10];
-    inicializa(20, tab);
-    preenche(10, tab, 0);
+int main (void){ 
+    Aluno* tab[10]; // declaração do vetor de ponteiros para Aluno com tamanho 10
+    inicializa(20, tab); // inicializa o vetor tab, def todas as posições como null
+    preenche(10, tab, 0); // preenche as informações dos alunos nas posições 0,1, 2
     preenche(10, tab, 1);
     preenche(10, tab, 2);
-    imprime_tudo(10, tab);
-    retira(10, tab, 0);
+    imprime_tudo(10, tab); // imprime as info dos alunos armazenados no vetor tab
+    retira(10, tab, 0); // faz a remoção dos alunos armazenados, que inicialmente foram colocados nas posições 0,1 e 2 do vetor
     retira(10, tab, 1);
     retira(10, tab, 2);
     return 0;
