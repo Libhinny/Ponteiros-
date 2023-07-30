dbfe15bdba4973908a300c82a908f7493cbc41cd
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 12:16:09 -0300

    Update pratica3.c

--- praticas-struct/pratica3.c
+++ praticas-struct/pratica3.c
@@ -1,17 +1,17 @@
 //Programa que recebe dados de uma pessoa, vetor de pessoas
 #include <stdlib.h>
 
-typedef struct pessoa {
+typedef struct pessoa { // struct que contém os campos de nome, idade, rg e altura de determinada pessoa
     char nome[20];
     int idade;
     int rg;
     float altura;
-}Pessoa;
+}Pessoa; // renomea com o comando typedef
 
 int main (void){
-    Pessoa var_pessoa[3];
+    Pessoa var_pessoa[3]; // declaração do vetor com tamanho 3 do tipo pessoa
 
-    int indice;
+    int indice; // laço que vai percorrer todo o vetor e pegará os dados informados
     for (indice=0; indice<3; indice++){
 
     printf("Informe o nome: ");
@@ -27,6 +27,7 @@ int main (void){
     scanf("%f", &var_pessoa[indice].altura );
     }
 
+    // imprime os dados informados 
     printf("Os dados informados:\n Nome: %s \t Idade: %d \t RG: %d \t Altura: %2.f ", var_pessoa.nome, var_pessoa.idade, var_pessoa.rg, var_pessoa.altura);
 
     return 0; 
