5779aa0240907beb40fc3c85a8c809b8e30dd537
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 23:18:55 -0300

    Update ex_union2.c

--- Union/ex_union2.c
+++ Union/ex_union2.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
-typedef union indice_a{
+typedef union indice_a{ // def. da union com os campos que informam os indices de determinado aluno
     float ig, ira, irt;
 }Indice_a;
 
-typedef struct aluno{
+typedef struct aluno{ // struct com os campos qeu informam os dados de determinado aluno
     char nome[20];
     int matricula;
     char curso[20];
     Indice_a indice;
 
-}Aluno;
+}Aluno; // renomeação da struct
 
 
 int main(void){
-    Aluno dados;
-    int op;
+    Aluno dados; // def. da variável dados do tipo Aluno
+    int op; // armazena a opção escolhida pelo user
 
+    // pega os dados informados pelo user e os armazena 
     printf("Digite o nome: ");
     scanf(" %[^\n]", dados.nome);
     printf("Digite a matricula: ");
     scanf("%d", &dados.matricula);
     printf("Digite o curso: ");
     scanf(" %[^\n]", dados.curso);
+
+    // solicita ao usuário que escolha o indice que deseja informar ao sistema
     printf("Digite: \n 0 - ig \n 1 - IRA\n 2 - IRT ");
     scanf("%d", &op);
+   
+    // trata da opção escolhida
     switch (op)
     {
     case 0:
