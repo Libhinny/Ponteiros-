9d87d7fd8465bdece8f42b9f89b7f2c5510b4c6d
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 23:10:10 -0300

    Update Questão2.c

--- Union/Struct_union_enum/Questão2.c
+++ Union/Struct_union_enum/Questão2.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-typedef union tipo{
+typedef union tipo{ // definindo a union agrupando diferentes tipos de produtos
     char alimento[20];
     char bebida[20];
     char eletronico[20];
 
 }Tipo;
 
-typedef struct produto{
+typedef struct produto{ // definição da struct com os campos de nome, preço e tipo do produto (que está dentro da union)
     char nome[20];
     float preco;
     Tipo tip;
 }Produto;
 
 int main (void){
-    Produto prod;
-    int op;
+    Produto prod; // armazena as informações do produto
+    int op; // armazena a opção escolhida
 
+
+    // armazena os dados fornecidos pelo user, bem como a op do tipo do produto
     printf("Digite o nome do produto: \n");
     scanf(" %[^\n]", prod.nome);
     printf("Digite o preco do produto: \n");
@@ -24,7 +26,7 @@ int main (void){
     printf("Escolha a opcão referente ao produto: \n 0 - Alimento \n 1 - Bebida \n 2 - Eletronico ");
     scanf("%d", &op);
 
-   switch (op)
+   switch (op) // trata da escolha do usuário e le a classe do produto
    {
        case 0:
        scanf(" %[^\n]", prod.tip.alimento);
@@ -39,12 +41,29 @@ int main (void){
        case 2:
        scanf(" %[^\n]", prod.tip.eletronico);
        printf("O produto é um eletronico \n");
-    default:
        break;
+       
+    default:
+        printf("Opção inválida\n");
+        return 1;
 
    }
 
-   printf ("Dados do produto: \n Nome: %s \n Preco: %f \n Classe do produto: %s \n", prod.nome, prod.preco, prod.tip); // corrigir a impressão dos dados
+   // impressão dos dados 
+   printf("Dados do produto: \n Nome: %s \n Preço: %f \n", prod.nome, prod.preco);
+
+   // imprime a classe de acordo com a opção escolhida
+   switch (op) {
+    case 0:
+        printf("Classe do produto: %s\n", prod.tip.alimento);
+        break;
+    case 1:
+        printf("Classe do produto: %s\n", prod.tip.bebida);
+        break;
+    case 2:
+        printf("Classe do produto: %s\n", prod.tip.eletronico);
+        break;
+    }
 
     return 0;
 }
