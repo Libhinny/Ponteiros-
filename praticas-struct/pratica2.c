1912ebaecac4aa46ff533f3e0ea9f669bb541293
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 12:13:47 -0300

    Update pratica2.c

--- praticas-struct/pratica2.c
+++ praticas-struct/pratica2.c
@@ -1,16 +1,17 @@
 //Programa que recebe dados de uma pessoa 
 #include <stdlib.h>
 
-typedef struct pessoa {
+typedef struct pessoa { // struct que tem os campos de dados de uma pessoa
     char nome[20];
     int idade;
     int rg;
     float altura;
-}Pessoa;
+}Pessoa; // renomea com o comando typedef
 
 int main (void){
-    Pessoa var_pessoa;
+    Pessoa var_pessoa; // declaração da variavel var_pessoa do tipo pessoa
 
+    // pega os dados informados e os armazena 
     printf("Informe o nome: ");
     scanf(" %[^\n]", var_pessoa.nome);
 
@@ -23,6 +24,7 @@ int main (void){
     printf("Digite a altura: ");
     scanf("%f", &var_pessoa.altura );
 
+    // imprime os dados informados na tela
     printf("Os dados informados:\n Nome: %s \t Idade: %d \t RG: %d \t Altura: %2.f ", var_pessoa.nome, var_pessoa.idade, var_pessoa.rg, var_pessoa.altura);
 
     return 0; 
