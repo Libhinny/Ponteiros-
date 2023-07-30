5f01d305d07c5701f0012e99a4bf990d80befad1
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-30 13:11:54 -0300

    Update struct_ponteiros2.c

--- praticas-struct/struct_ponteiros2.c
+++ praticas-struct/struct_ponteiros2.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct disciplina{   // Struct e Ponteiros
+// Struct e Ponteiros
+
+typedef struct disciplina{   // def a struct com os campos de nome da disciplina e a nota
     char nome[30];
     float nota;
-}Disciplina;
+}Disciplina; // renomea com o comando typedef
 
-typedef struct aluno {
+typedef struct aluno { // def a struct com os campos de nome e idade do aluno
     char nome[20];
     int idade;
-} Aluno;
+} Aluno; // renomea com o comando typedef
 
 int main (void){
-    Aluno * aluno = (Aluno *) malloc(sizeof(Aluno));
-    if (aluno == NULL){
+    Aluno * aluno = (Aluno *) malloc(sizeof(Aluno)); // faz a alocação dinamica 
+    if (aluno == NULL){ // verifica se houve erros na alocação
         printf("Sem memória!\n");
         exit(1);
     }
 
+    // pega os dados informados e os armazena 
     printf("Digite o nome: ");
     scanf(" %[^\n]", aluno->nome );
 
