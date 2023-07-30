410896c10ac5cf9da4d4681c2d25a58cd055be2a
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-07-29 22:48:43 -0300

    Update Questão1.c

--- Union/Struct_union_enum/Questão1.c
+++ Union/Struct_union_enum/Questão1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-typedef struct pessoa {
+enum genero {feminino, masculino};
+
+typedef enum genero Genero;
+
+typedef struct pessoa { // definição da estrutura struct com os parametros de nome, idade e genero
     char nome[20];
     int idade;
     Genero genero[20];
 }Pessoa;
-
-enum genero {feminino, masculino};
-
-typedef enum genero Genero;
 
 int main (void){
 
@@ -19,17 +19,16 @@ int main (void){
     scanf(" %[^\n]", p.nome);
     printf("Digite sua idade: \n");
     scanf("%d", &p.idade);
-    printf("Digite seu gênero: \n");
-    scanf(" %[^\n]", p.genero);
 
-    printf("Digite 0 ou 1: ");
+    printf("Informe seu gênero (0 para feminino e 1 para masculino): \n");
     scanf("%d", &f_m);
+   
     printf("Valor informado: %d", &f_m);
 
     if (f_m == feminino){
         printf("Feminino");
     } else if (f_m == masculino){
-        prinft("Masculino");
+        printf("Masculino");
     }
     else {
         printf("Erro");
